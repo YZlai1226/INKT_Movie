f9d159635db8aa80b1f22667c5d73dd9ea1ced95
Karen <yong-zhen.lai@epitech.eu>
2022-05-12 10:20:30 +0200

    add image to movies

--- inkt_front/components/homecomps/EachMovie.js
+++ inkt_front/components/homecomps/EachMovie.js
@@ -1,6 +1,10 @@
-export default function EachMovie (props, image) {
+import Image from "next/image"
+
+export default function EachMovie (props) {
+    const image = "https://image.tmdb.org/t/p/original" + props.movie.poster_path
     return (
     <div>
+          <Image src={image} alt="movie poster" width={170} height={210} layout="fixed" />
           <p>Name: {props.movie.title}</p>
           <p>Released_date: {props.movie.release_date}</p>
           <p>Rating: {props.movie.vote_average}</p>
