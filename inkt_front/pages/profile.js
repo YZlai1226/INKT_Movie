165d16de6e97a1dcf1535be4dee3e2bed1c64226
Nicolas Bretécher <bretechern@gmail.com>
2022-05-14 17:56:32 +0200

    fix index.js

--- inkt_front/pages/profile.js
+++ inkt_front/pages/profile.js
@@ -1,5 +1,23 @@
+import axios from 'lib/axios';
+import useState from 'react';
+
 export default function Profile() {
+    /* const [user, setUser] = useState(null); */
+    const [loading, setLoading] = useState(false);
+    /* const [auth, useAuth] = useAuth(); */
+    const { user } = useAuth({ middleware: 'guest' })
+
+    /* useEffect(
+        axios.get('/api/user/')
+        .then((res) => setUser(res))
+        .then (() => setLoading(false))
+    ) */
+
     return (
-        <div>I am in profile page!!</div>
+        <div>
+            <p>
+                {loading ? 'Loading your info...' : user}
+            </p>
+        </div>
     )
 }
